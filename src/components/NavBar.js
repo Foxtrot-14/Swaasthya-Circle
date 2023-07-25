1dee8ec63d4e2a229692e1ec91ee064f54576e86
Noaman <noamanahmedai@gmail.com>
2023-07-25 15:46:03 +0530

    'navbar'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,55 +1,32 @@
 import React, { useState } from "react";
+import "./NavBar.css";
 import { Link } from "react-router-dom";
 const NavBar = () => {
-  const [click, setClick] = useState(false);
-  const handleClick = () => {
-    setClick(!click);
-  };
-  const closeMobileMenu = () => {
-    setClick(false);
-  };
+  // const [click, setClick] = useState(false);
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            Nav
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
-          </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                Home
-              </Link>
+      <div class="menu">
+        <center>
+          <i>s</i>
+        </center>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
             </li>
-            <li className="nav-item">
-              <Link
-                to="/activities"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                Activities
-              </Link>
+            <li>
+              <a href="about">About Us</a>
             </li>
-            <li className="nav-item">
-              <Link
-                to="/volunteer"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
-                Volunteer
-              </Link>
+            <li>
+              <a href="activities">Activities</a>
             </li>
-            <li className="nav-item">
-              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
-                About Us
-              </Link>
+            <li>
+              <a href="volunteer">Contacts</a>
             </li>
           </ul>
-        </div>
-      </nav>
+        </nav>
+      </div>
+      <div class="container"></div>
     </>
   );
 };
