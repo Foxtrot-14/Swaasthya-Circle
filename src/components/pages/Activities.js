60d995269a3394bd8342d2bf4a3084aa7569b366
Noaman <noamanahmedai@gmail.com>
2023-08-02 14:02:57 +0530

    home-page-fixed

--- src/components/pages/Activities.js
+++ src/components/pages/Activities.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Activity from "../Activity";
-import "./Activities.css";
 const Activities = () => {
   return (
     <>
       <div className="act">
         <center>
           <h1>Our Journey So Far...</h1>
-          <Activity />
         </center>
       </div>
     </>
